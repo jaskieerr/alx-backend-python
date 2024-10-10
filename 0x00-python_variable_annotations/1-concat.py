f3c02648194da7de4270cdbc14f91111be3ab0be
jaskieerr <nizar.zairat@gmail.com>
2024-10-10 19:22:42 +0000

    ss

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+concatenates str1+str2
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    the function that concatenates
+    this is izi and fucking boring
+    '''
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import math
+'''
+floors a float
+'''
+
+
+def floor(n: float) -> int:
+    '''
+    flooring float function
+    '''
+    return math.floor(n)
