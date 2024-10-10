280e9a73fce7494c7f26784921989ec6135d6c6a
jaskieerr <nizar.zairat@gmail.com>
2024-10-10 19:35:01 +0000

    4n5

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+bla bla bla bla bla
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    bla bla bla bla bla
+    '''
+    return str(n)
---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+just defining i guess
+'''
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
